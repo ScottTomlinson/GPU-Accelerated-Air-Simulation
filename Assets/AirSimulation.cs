7271d80f95e01f5a8948b72d16f3734ce5809850
Scott <frankstomlinson@gmail.com>
2017-05-09 19:16:33 -0400

    Rogue Code Removal
    readability improvements

--- Assets/AirSimulation.cs
+++ Assets/AirSimulation.cs
@@ -43,10 +43,11 @@ public class AirSimulation : MonoBehaviour {
         updateCounter++;
         if(updateCounter > getDataInterval)
         {
+            //move RunAirSim() to outside of  updateCounter if statement to run every frame, but only update graphics every n frames
             RunAirSim();
             airBuffer.GetData(outputData);
-            int randomIndex = (int)Random.Range(0f, (float)outputData.Length + 1f);
-            //outputData[randomIndex] = 20000000f;
+            //int randomIndex = (int)Random.Range(0f, (float)outputData.Length + 1f);
+            //outputData[randomIndex] = 200f;
             visualBuffer.SetData(outputData);
             //airBuffer.SetData(outputData);
             updateCounter = 0;
@@ -55,45 +56,8 @@ public class AirSimulation : MonoBehaviour {
 
     void SetupAirSim()
     {
-
-        //tell the compute shader how many thread groups we're gonna dispatch
-        int[] nodeCount = new int[1];
-        nodeCount[0] = numNodes;
-        int kNum = airShader.FindKernel("SetAirNodeQuantity");
-        ComputeBuffer nodeCountBuffer = new ComputeBuffer(1, sizeof(int));
-        nodeCountBuffer.SetData(nodeCount);
-        airShader.SetBuffer(kNum, "nodeCountBuffer", nodeCountBuffer);
-        airShader.Dispatch(kNum, 1, 1, 1);
-
-        int[] dispatchSize = new int[3];
-        for(int i = 0; i < 3; i++)
-        {
-            dispatchSize[i] = cubeSize;
-        }
-        kNum = airShader.FindKernel("SetDispatchSize");
-        ComputeBuffer dispatchSizeBuffer = new ComputeBuffer(3, sizeof(int));
-        dispatchSizeBuffer.SetData(dispatchSize);
-        airShader.SetBuffer(kNum, "dispatchSizeBuffer", dispatchSizeBuffer);
-        airShader.Dispatch(kNum, 1, 1, 1);
-
-
-        //airShader.SetInt("numNodes", numNodes);
-        //airShader.SetInts("dispatchSize", dispatchSize);
-
-        airShader.SetInt("width", 10 * cubeSize);
-        airShader.SetInt("height", 10 * cubeSize);
-        airShader.SetInt("depth", 10 * cubeSize);
-
-        //make sure that shit worked
-        CheckAirSimData(nodeCountBuffer, dispatchSizeBuffer);
-
         //make input array
         inputData = new float[numNodes];
-        for(int i = 0; i < numNodes; i++)
-        {//randomize input data
-            //inputData[i] = Random.Range(0f, 50f);
-            inputData[i] = 0f;
-        }
 
         ///////////////////
         //**FOR TESTING**//
@@ -102,8 +66,8 @@ public class AirSimulation : MonoBehaviour {
         int _x = 0;
         int _y = 0;
         int _z = 0;
-        int centerIndex = _x + (_z * 100) + (_y * 100 * 100);
-        inputData[centerIndex] = 50000000f;
+        int desiredIndex = _x + (_z * 100) + (_y * 100 * 100);
+        inputData[desiredIndex] = 50000000f;
 
         //make output array
         outputData = new float[numNodes];
@@ -114,7 +78,12 @@ public class AirSimulation : MonoBehaviour {
         //make a buffer and set the input
         airBuffer = new ComputeBuffer(numNodes, sizeof(float));
         airBuffer.SetData(inputData);
-
+        
+        //tell the compute shader important info
+        airShader.SetInt("numNodes", numNodes);
+        airShader.SetInt("width", 10 * cubeSize);
+        airShader.SetInt("height", 10 * cubeSize);
+        airShader.SetInt("depth", 10 * cubeSize);
 
         //set the RWStructuredBuffer in the compute shader to match up with our airBuffer here
         airShader.SetBuffer(kernalOne, "airBuffer", airBuffer);
@@ -130,16 +99,6 @@ public class AirSimulation : MonoBehaviour {
     void RunAirSim()
     {
         airShader.Dispatch(kernalOne, cubeSize, cubeSize, cubeSize);
-    }
-
-    void CheckAirSimData(ComputeBuffer nBuff, ComputeBuffer dBuff)
-    {
-        int[] _nodeCount = new int[1];
-        int[] _dispatchSize = new int[3];
-        nBuff.GetData(_nodeCount);
-        dBuff.GetData(_dispatchSize);
-        Debug.Log("Node count from GPU: " + _nodeCount[0]);
-        Debug.Log("Dispatch size from GPU: x:" + _dispatchSize[0] + " y:" + _dispatchSize[1] + " z:" + _dispatchSize[2]);
     }
 
     void OnRenderObject()
